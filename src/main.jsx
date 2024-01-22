dc08bfbe5504cba104e49ae8ba5dd55ed6010edc
HildanRizkiBV2 <hildanrizki16@gmail.com>
2024-01-22 19:37:41 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
-import './dist/css/main.css'
+import './main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
