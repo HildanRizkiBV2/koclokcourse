85771bd8861810c795e9dd8e19b0e1deccce18d9
HildanRizkiBV2 <hildanrizki16@gmail.com>
2024-01-22 20:37:28 +0700

    cek

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
         <Container>
          <Row className="header-box d-flex align-items-center">
           <Col lg="6">
-            <h1>A Im <Typewriter options={{ strings:['Hildan', 'Developer','Front-end'],autoStart: true,loop: true,}}/>
+            <h1>I am <Typewriter options={{ strings:['Hildan', 'Developer','Front-end'],autoStart: true,loop: true,}}/>
             </h1>
             <h1 className="mb-2">belajar <span>cooding </span> di sini</h1>
             <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quae sed perferendis recusandae obcaecati quos.</p>
